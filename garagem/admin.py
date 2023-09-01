476983970f28b8493660687a66649f3ef28bcbd1
brunamy <bm6952@gmail.com>
2023-09-01 14:11:37 -0300

    first commit

---
+++ garagem/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.contrib import admin
+from django.contrib import admin
+from garagem.models import Acessorio, Categoria
+
+
+admin.site.register(Acessorio)
+admin.site.register(Categoria)
